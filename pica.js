de64b79626b01d4e86fa6c1dc73431b01839cc3c
Jabes-Lima <111255266+Jabes-Lima@users.noreply.github.com>
2022-10-31 13:42:27 -0300

    feito

--- pica.js
+++ pica.js
@@ -4,16 +4,16 @@ function verificar(){
     var res = document.querySelector('div#res')
     if(cm.value <= 0){
         window.alert('Não existe pal com 0 ou menor cm. verifique os dados e tente novamente!')
-    }else if(cm.value > 0 && cm.value < 7 ){
-        res.innerHTML = `Seu pal e De ${cm.value} cm. ta muito pequeno se mate!`
-    }else if(cm.value > 6 && cm.value < 15){
-        res.innerHTML = `seu pal e de ${cm.value} cm. ta pequeno porem da pra sobreviver!`
-    }else if(cm.value > 14 && cm.value < 21){
-        res.innerHTML = `Seu pal e De ${cm.value} cm. Seu pal e grande mais voçe e broxa e precoçe!`
+    }else if(cm.value > 0 && cm.value < 10 ){
+        res.innerHTML = `Seu pal e extremamente pequeno🤢 Se mate!🤬`
+    }else if(cm.value > 9 && cm.value < 16){
+        res.innerHTML = `Seu pal e Pequeno, porem dar pra sobreviver😴`
+    }else if(cm.value > 15 && cm.value < 21){
+        res.innerHTML = `Seu Pal e grande Parabens👏🏻`
     }else if(cm.value > 20 && cm.value < 33){
-        res.innerHTML = 'Parabens vc e um kid bengala da vida!'
+        res.innerHTML = 'Esta na medida de um Ator Porno🍷🗿'
     }else{
-        res.innerHTML = 'voçe e um Deus!'
+        res.innerHTML = 'Voçe e um Roludo 🍷🗿'
     }
 
 }
